d2350c10f542d6897025540046da5b880f97fd4c
Chandra Bhushan <chandrabhushanbgp321@gmail.com>
2025-02-18 02:40:28 +0530

    ui in header and app sidebar

--- components/custom/AppSideBar.jsx
+++ components/custom/AppSideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   Sidebar,
   SidebarContent,
@@ -8,26 +8,51 @@ import {
 } from "@/components/ui/sidebar";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { HelpCircle, Icon, LogOut, MessageCircleCode, Settings, Wallet } from "lucide-react";
+import {
+  HelpCircle,
+  Icon,
+  LogOut,
+  MessageCircleCode,
+  Settings,
+  Wallet,
+} from "lucide-react";
 import WorkspaceHistory from "./WorkspaceHistory";
 import SideBarFooter from "./SideBarFooter";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 const AppSideBar = () => {
-    
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  userDetail && console.log(userDetail.name);
+
   return (
-    <Sidebar className='h-full'>
-      <SidebarHeader className="p-5">
-        <Image src={"/ThunderLogo.png"} alt="logo" width={30} height={30} />
-        <Button className='mt-5'>
+    <Sidebar className="h-full">
+      <SidebarHeader className="p-1">
+        <div className="flex items-center gap-3 mt-5">
+          <Image
+            src="/ThunderLogo.png"
+            alt="logo"
+            width={40}
+            height={40}
+            
+          />
+          <div className="text-white-400">
+            <h2 className="text-lg font-semibold ">
+              Hello, <br />
+              <span >{userDetail?.name}</span>
+            </h2>
+          </div>
+        </div>
+
+        <Button className="mx-5">
           <MessageCircleCode /> Start new chat
         </Button>
       </SidebarHeader>
-      <SidebarContent className="p-5">
-        <SidebarGroup />
+      <SidebarContent className="p-5 mt-1 scrollbar-hide">
+        {/* <SidebarGroup /> */}
         <WorkspaceHistory></WorkspaceHistory>
-        <SidebarGroup />
+        {/* <SidebarGroup /> */}
       </SidebarContent>
-      <SidebarFooter> 
+      <SidebarFooter>
         <SideBarFooter />
       </SidebarFooter>
     </Sidebar>
--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -2,21 +2,24 @@ import Image from "next/image";
 import React, { useContext } from "react";
 import { Button } from "../ui/button";
 import { UserDetailContext } from "@/context/UserDetailContext";
+import Link from "next/link";
 
 const Header = () => {
   const { userDetail, setUserDetail } = useContext(UserDetailContext);
 
   return (
     <div className="p-4 flex justify-between items-center">
+      <Link href='/'>
       <div className="flex gap-2 items-center font-serif text-2xl">
         <Image
           src={"/ThunderLogo.png"}
           alt="logo"
           width={40}
           height={40}
-        ></Image>
+          ></Image>
         <h1>Ai App Maker</h1>
       </div>
+      </Link>
 
       {!userDetail && (
         <div className="flex gap-5">
